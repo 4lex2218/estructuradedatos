f886c4a10a75cb80e6a16c66724c8915b4772d93
4lex2218 <76576162+4lex2218@users.noreply.github.com>
2026-02-09 21:57:52 -0500

    2.7
    ejercicio 2.7 es el que busca si uno es el resultado de la suma de los otros dos

---
+++ guia 1/2.7 .py
@@ -0,0 +1,17 @@
+def verificar_suma_de_dos():
+    print("2Uno es la suma de los otros dos?")
+    
+    a = float(input("Ingrese el primer número: "))
+    b = float(input("Ingrese el segundo número: "))
+    c = float(input("Ingrese el tercer número: "))
+ 
+    if a + b == c:
+        return f"Si {a} + {b} = {c}"
+    elif a + c == b:
+        return f"Si {a} + {c} = {b}"
+    elif b + c == a:
+        return f"Si {b} + {c} = {a}"
+    else:
+        return "No hay ningún número que sea la suma de los otros dos."
+    
+print(verificar_suma_de_dos())
